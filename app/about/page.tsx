8304e313b3f9f7d6a3d6ce4ceb305f199c1dfaae
whaus777 <whaus777@gmail.com>
2025-04-23 19:23:32 -0700

    add page title

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,14 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'About',
+  description: 'My personal website',
+}
+
 export default function About() {
   return (
     <div className="w-full max-w-[900]">
+      <h1>{`${metadata.title}`}</h1>
       <div>about me</div>
       <div>wow much content</div>
       <div>wow much content</div>
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,14 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'My personal website',
+}
+
 export default function Blog() {
   return (
     <div className="w-full max-w-[900]">
+      <h1>{`${metadata.title}`}</h1>
       <div>first blog</div>
       <div>first blog</div>
       <div>first blog</div>
